c5ddad42ecb1a541a9b94ebbc303b45a74ace091
90536 <90536@DESKTOP-04SRGOA>
2022-02-14 15:32:22 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    /// <summary>
+    /// bir iş sınıfı başka bir sınıfı new'lemez
+    /// </summary>
+    /// 
+
+    //burası bussiness zamanla iş kodları ile data access kodları 
+    //birbirinden ayrılabilirler bu yüzden "IEntityRepository" değil de
+    // "IProductService" i inherit ettik.
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //iş kodları
+            //yetkisi var mı? if vs vs
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p=>p.CategoryId==id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=>p.UnitPrice>=min && p.UnitPrice<=max);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,66 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            //oracle,sql server dan geliyor gibi simüle edicez
+            _products = new List<Product>
+            {
+                new Product{ProductId=1, ProductName="bardak",UnitPrice=15,UnitsInStock=15 },
+                new Product{ProductId=2, ProductName="kamera",UnitPrice=500,UnitsInStock=3 },
+                new Product{ProductId=3, ProductName="telefon",UnitPrice=1500,UnitsInStock=2 },
+                new Product{ProductId=4, ProductName="klavye",UnitPrice=150,UnitsInStock=65 },
+                new Product{ProductId=5, ProductName="fare",UnitPrice=85,UnitsInStock=1},
+
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //p=> lambda anlamına geliyor
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            _products.Add(productUpdate);
+        }
+    }
+}
