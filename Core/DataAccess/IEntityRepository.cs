ba50b56a8a6c710156438362b5a72f12ed11ba66
Deniz Türk <deniz.turk@ogr.gsu.edu.tr>
2022-02-15 16:43:20 +0300

    v_0.9

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,10 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //sınırlandırma generic constraint 
     // T:class --->>>> T referans tip olmalı demek class demek degil
@@ -19,7 +19,7 @@ namespace DataAccess.Abstract
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
-        List<T> GetAllByCategory(int entity);
+  
 
     }
 }
