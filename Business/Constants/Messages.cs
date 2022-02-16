ffa88ab3167d0424e0f34a2f214ac03fdcd796cb
Deniz Türk <deniz.turk@ogr.gsu.edu.tr>
2022-02-16 23:38:18 +0300

    v_1.0

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        //intro level ? multi language sport??
+       
+        public static string ProductAdded = "Product added";
+        public static string ProductNameInvalid= "Product name is invalid";
+        public static string MaintenanceTime="Maintenance Time";
+        public static string ProductsListed ="Products Listed";
+    }
+}
